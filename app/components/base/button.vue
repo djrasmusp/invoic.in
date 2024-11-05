7e9beb5a143a77050b1f46b0afdc5f3858652ea8
Rasmus P <djrasmusp@users.noreply.github.com>
2024-11-05 23:25:27 +0100

    feature: added test

---
+++ app/components/base/button.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+type Button = {
+  type: 'primary' | 'secondary' | 'simple'
+  label: string
+  icon?: string | undefined
+  state?: 'loading' | undefined
+  href?: string | undefined
+  target?: '_self' | '_blank'
+}
+
+withDefaults(defineProps<Button>(), {
+  type: 'primary',
+  label: '',
+  icon: undefined,
+  state: undefined,
+  href: undefined,
+  target: '_self'
+})
+
+</script>
+
+<template>
+  <div>
+    <NuxtLink v-if="href" :to="href" :target>{{ label }}</NuxtLink>
+    <button v-else>{{ label}}</button>
+  </div>
+</template>
+
+<style scoped></style>
