d3754cb85c4791dadb9f52feca2eb3ad33d14019
Rasmus P <djrasmusp@users.noreply.github.com>
2025-03-01 22:36:29 +0100

    feature(9): Add eslint and prettier

---
+++ .eslintrc.mjs
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: [
+        '@nuxt/eslint-config',
+        'plugin:vue/recommended',
+        'plugin:prettier-vue/recommended',
+        'plugin:prettier/recommended',
+        'plugin:storybook/recommended',
+    ],
+}
